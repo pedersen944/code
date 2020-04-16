47ce6ba7631acb48fe871d231b7cf270b3e0579e
pedersen944 <mpeders99@gmail.com>
2020-04-16 09:51:25 +0200

    Syretest

---
+++ JavaScript/Qunit - unittests/gjenkjenne epost/tests.js
@@ -0,0 +1,17 @@
+function isEmail(txt) {
+   
+    if 
+   (txt.indexOf('@'));
+    return true ;
+    }
+    
+    function isEmailSpacedOut(txt){
+        if(txt.indexOf(' ') >= 0){
+            console.log("contains spaces");
+        
+            txt.trim();
+            return txt.trim();
+        }
+
+    }
+    
